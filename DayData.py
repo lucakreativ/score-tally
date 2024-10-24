3bc284c99f9982e8249212e7de7139fcf9926b36
lucakreativ <lucaeckenfels@gmx.de>
2024-10-24 16:49:07 +0200

    bugfix background color

--- DayData.py
+++ DayData.py
@@ -19,6 +19,6 @@ class DayData:
 
     def export_to_dict(self):
         if self.exists:
-            return {'humanTime': self.convert_to_human_time(), 'value': self.value, "activitylevel": min(math.ceil(self.value), 4)}
+            return {'humanTime': self.convert_to_human_time(), 'value': self.value, "activitylevel": str(min(math.floor(self.value), 3))}
         else:
             return {'humanTime': '', 'value': 0}
